29d4ffcd1893d2d25068f2e9f1fe9bb0d6359865
pengda <3053787951@qq.com>
2023-02-11 17:09:41 +0800

    docs: 完成parse 函数

--- src/core/shadowsocks.rs
+++ src/core/shadowsocks.rs
@@ -1,12 +1,13 @@
-use anyhow::Result;
+use crate::common::UriQueries;
+use anyhow::{Context, Result};
 
 pub struct Shadowsocks {
-    name: String,
+    name: Option<String>,
     server: String,
-    port: String,
+    port: i32,
     password: String,
     cipher: String,
-    pulgin: Sip003,
+    plugin: Option<Sip003>,
 }
 
 impl Shadowsocks {
@@ -16,6 +17,9 @@ impl Shadowsocks {
             use http::uri::Uri;
 
             let link = s.parse::<Uri>().ok()?;
+
+            let server = link.host().map(String::from)?;
+
             let auth = link.authority()?;
             let username = auth.as_str().split('@').next()?;
             let username =
@@ -23,10 +27,25 @@ impl Shadowsocks {
 
             let mut username = username.split(':');
 
-            let cipher = username.next()?;
-            let password = username.next()?;
+            let cipher = username.next().map(String::from)?;
+            let password = username.next().map(String::from)?;
 
-            unimplemented!()
+            let sip003 = link
+                .query()
+                .and_then(|q| q.parse::<UriQueries>().ok())
+                .and_then(|q| q.get("plugin").and_then(|v| v.first().cloned()))
+                .and_then(|plugin_str| Sip003::from_plugin_str(&plugin_str));
+
+            let port = link.port().and_then(|p| p.as_str().parse::<i32>().ok())?;
+
+            Some(Shadowsocks {
+                name: s.split('#').skip(1).next().map(String::from),
+                server,
+                port,
+                password,
+                cipher,
+                plugin: sip003,
+            })
         }
 
         unimplemented!()
@@ -38,10 +57,61 @@ struct Sip003 {
     opts: Sip003Opts,
 }
 
+impl Sip003 {
+    fn from_plugin_str(s: &str) -> Option<Self> {
+        let fld: Vec<_> = s.splitn(2, ';').collect();
+        let name = match *fld.first()? {
+            "obfs-local" | "simpleobfs" => String::from("simple-obfs"),
+            x => x.to_string(),
+        };
+
+        Some(Self {
+            name,
+            opts: Sip003Opts::from_plugin_str(fld.get(1)?),
+        })
+    }
+}
+
+#[derive(Default)]
 struct Sip003Opts {
     tls: String,
     obfs: String,
     host: String,
     path: String,
     impf: String,
+}
+
+impl Sip003Opts {
+    fn from_plugin_str(s: &str) -> Self {
+        let mut opts = Self::default();
+
+        for item in s.split(';') {
+            let vals = item.splitn(2, '=').collect::<Vec<_>>();
+
+            let a = vals[0];
+            let b = || {
+                vals.get(1)
+                    .map(|v| String::from(*v))
+                    .unwrap_or_else(String::new)
+            };
+
+            match a {
+                "tls" => opts.tls = String::from("tls"),
+                "obfs" | "mode" => opts.obfs = b(),
+                "obfs-path" | "obfs-uri" | "path" => {
+                    let b = b();
+                    if b.starts_with('/') {
+                        opts.path = b
+                    } else {
+                        opts.path = format!("/{b}")
+                    }
+                }
+                "obfs-host" | "host" => opts.host = b(),
+                "impl" => opts.impf = b(),
+                _ => continue,
+            }
+        }
+
+        opts
+    }
 }
